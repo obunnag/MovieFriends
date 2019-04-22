d105fb49810f38a59bb494064ed60a3da88cf8f2
Opal Bunnag <Opal@opals-macbook-pro.local>
2019-04-21 23:34:10 -0500

    Initial Commit

---
+++ MovieFriends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieFriends
+//
+//  Created by Opal Bunnag on 4/21/19.
+//  Copyright © 2019 pb23656. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
