5f308267b6bb3037bb93ebda29b9540ecec8cc08
Group 8 <9kimgu@gmail.com>
2019-05-06 04:17:55 -0500

    updated HomeView to update button images

--- MovieFriends/HomeViewController.swift
+++ MovieFriends/HomeViewController.swift
@@ -11,7 +11,10 @@ import UIKit
 class HomeViewController: UIViewController {
     
     
-    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
     
     let baseURL = "https://api.themoviedb.org/3/trending/movie/week?api_key="
     let apiKey = "0518529e7e2dd0cf9c39e55884e0a084"
@@ -39,14 +42,31 @@ class HomeViewController: UIViewController {
                    
                     let swiftyJSON = try JSON(data: data!)
                     let theTitle = swiftyJSON["results"].arrayValue
-                    let theImage = "https://image.tmdb.org/t/p/w500" + swiftyJSON["results"][0]["poster_path"].string!
-                    print(theImage)
-                    let theImageURL = URL(string: theImage)
+                    let theImage1 = "https://image.tmdb.org/t/p/w500" + swiftyJSON["results"][0]["poster_path"].string!
+                    let theImage2 = "https://image.tmdb.org/t/p/w500" + swiftyJSON["results"][1]["poster_path"].string!
+                    let theImage3 = "https://image.tmdb.org/t/p/w500" + swiftyJSON["results"][2]["poster_path"].string!
+                    let theImage4 = "https://image.tmdb.org/t/p/w500" + swiftyJSON["results"][3]["poster_path"].string!
+                    print(theImage1)
+                    let theImageURL1 = URL(string: theImage1)
+                    let theImageURL2 = URL(string: theImage2)
+                    let theImageURL3 = URL(string: theImage3)
+                    let theImageURL4 = URL(string: theImage4)
                     print("WAter is good")
                     DispatchQueue.main.async{
-                        if let ImageData = NSData(contentsOf: theImageURL!) {
-                            self.image1.image = UIImage(data: ImageData as Data)
-                        
+                        if let ImageData1 = NSData(contentsOf: theImageURL1!) {
+                            self.button1.setImage(UIImage(data: ImageData1 as Data), for: .normal)
+                            print("The image loaded")
+                        }
+                        if let ImageData2 = NSData(contentsOf: theImageURL2!) {
+                            self.button2.setImage(UIImage(data: ImageData2 as Data), for: .normal)
+                            print("The image loaded")
+                        }
+                        if let ImageData3 = NSData(contentsOf: theImageURL3!) {
+                            self.button3.setImage(UIImage(data: ImageData3 as Data), for: .normal)
+                            print("The image loaded")
+                        }
+                        if let ImageData4 = NSData(contentsOf: theImageURL4!) {
+                            self.button4.setImage(UIImage(data: ImageData4 as Data), for: .normal)
                             print("The image loaded")
                         }
                     }
@@ -59,7 +79,6 @@ class HomeViewController: UIViewController {
                     let theTest = "Chicken nugget"
                     let theTest2 = "Water cup"
                     //print(theTest2)
-                    print(theImage)
                     //print(theTest)
                     
                     
